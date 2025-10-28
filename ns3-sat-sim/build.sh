6a945da6551efcef9800ddf555aeaedecad82c09
pflin <welin9211@gmail.com>
2025-10-28 15:27:07 +0800

    fix: add CXXFLAGS to optimized build configuration to suppress warnings

--- ns3-sat-sim/build.sh
+++ ns3-sat-sim/build.sh
@@ -25,7 +25,7 @@ elif [ "$1" == "--debug_minimal" ]; then
   ./waf configure --build-profile=debug --enable-mpi --out=build/debug_minimal || exit 1
 
 elif [ "$1" == "--optimized" ]; then
-  ./waf configure --build-profile=optimized --enable-mpi --out=build/optimized || exit 1
+  ./waf configure --build-profile=optimized --enable-mpi --out=build/optimized CXXFLAGS="-Wno-maybe-uninitialized" || exit 1
 
 elif [ "$1" == "--optimized_with_tests" ]; then
   ./waf configure --build-profile=optimized --enable-mpi --enable-tests --out=build/optimized_with_tests || exit 1
